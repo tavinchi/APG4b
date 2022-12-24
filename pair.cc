be3947b4359b933cb6acc92c4c6ef7779c85fb16
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2022-12-24 13:19:04 +0900

    pair

--- pair.cc
+++ pair.cc
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+	/*
 	pair<int,bool> p(1000,false);
 	cout <<p.second<<endl;
 
@@ -11,4 +12,12 @@ int main(){
 	bool b;
 	tie(a,b)=p;
 	cout <<a<<endl;
+*/
+	pair<int,int> a(3,1);
+	pair<int,int> b(3,10);
+	if(a<b){
+		cout <<"bのほうがでかい";
+	}else{
+		cout <<"aのほうがでかい";
+	}
 }
