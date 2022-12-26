ed386667d3382718a446ac5b046c56d439f55330
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2022-12-26 10:14:54 +0900

    pair

--- pair.cc
+++ pair.cc
@@ -1,23 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	/*
-	pair<int,bool> p(1000,false);
+	pair<int,string> p(3,"gad");
 	cout <<p.second<<endl;
-
-	p=make_pair(100,true);
-	cout <<p.second<<endl;
-
-	int a;
-	bool b;
-	tie(a,b)=p;
-	cout <<a<<endl;
-*/
-	pair<int,int> a(3,1);
-	pair<int,int> b(3,10);
-	if(a<b){
-		cout <<"bのほうがでかい";
-	}else{
-		cout <<"aのほうがでかい";
-	}
+	
+	
 }
