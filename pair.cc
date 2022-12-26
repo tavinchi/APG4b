f099867da4d662383b9d9b8fccadacbd631f3527
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2022-12-26 20:49:45 +0900

    Z-pair.cc

--- pair.cc
+++ pair.cc
@@ -1,8 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+	/*
 	pair<int,string> p(3,"gad");
 	cout <<p.second<<endl;
 	
-	
+	p.first=4;
+	cout <<p.first<<endl;
+
+	p=make_pair(10,"ggggg");
+	int a;
+	string s;
+	tie(a,s)=p;
+	cout <<a<<endl;
+	cout <<s<<endl;
+	*/
+	pair<int,int> a(3,5);
+	pair<int,int> b(3,7);
+
+	if(a<b){
+		cout <<"b";
+	}else{
+		cout <<"a";
+	}
+
 }
