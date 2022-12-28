ec92cfb68d0fe4bc59604c059f3b891188b8de4d
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2022-12-28 15:35:55 +0900

    AA-map

---
+++ map.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	map<string,int> score;
+	score["matsu"]=80;
+	score["ritsu"]=99;
+	cout <<score.at("matsu")<<endl;
+	cout <<score.size()<<endl;
+	score.erase("ritsu");
+	cout <<score.size()<<endl;
+	score["katsu"]=99;
+	if(score.count("matsu")){
+		cout <<score.at("matsu")<<endl;
+	}
+}
